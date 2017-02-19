e9ddb613812cba474d2b05097f8309f84cf7194e
Abdul Rehman Khan <arkspeed@gmail.com>
2017-02-19 20:14:48 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+# Data Preprocessing Template
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
