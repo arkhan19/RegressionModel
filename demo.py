e5545eb29a475438b90c21ba5a100a3337c95205
Abdul Rehman Khan <arkspeed@gmail.com>
2017-02-19 20:15:20 +0530

    Importing Dataset

--- demo.py
+++ demo.py
@@ -6,3 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+# Importing the dataset
+dataset = pd.read_csv('/Users/f3n1Xx/Documents/PycharmProjects/RegressionModel/Salary_Data.csv')
+X = dataset.iloc[:, 0].values
+Y = dataset.iloc[:, 1].values
+
