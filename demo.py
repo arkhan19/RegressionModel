4888e89de7fe9df8aa70bfa18ef49e4940b70402
Abdul Rehman Khan <arkspeed@gmail.com>
2017-02-19 20:35:34 +0530

    5. Visualize Training and Test Results.

--- demo.py
+++ demo.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 # Importing the dataset
 dataset = pd.read_csv('/Users/f3n1Xx/Documents/PycharmProjects/RegressionModel/Salary_Data.csv')
@@ -27,3 +28,21 @@ y_train = sc_y.fit_transform(y_train)"""
 # Linear Regression
 reg = LinearRegression()
 reg.fit(X_train, Y_train)
+
+#Predicting the Test Set Results
+sal = reg.predict(X_test)
+
+#Visualising the Training and Test set result
+plt.scatter(X_train,Y_train, color = 'red')
+plt.plot(X_train, reg.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X_test,Y_test, color = 'red')
+plt.plot(X_train, reg.predict(X_train), color = 'black')
+plt.title('Salary vs Experience (Testing Set')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
